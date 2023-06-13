afaa2598878f2a64588562ad6ad433c121c879c4
Manuel Cota <56351434+mannynacc@users.noreply.github.com>
2023-06-13 13:05:14 -0600

    fix redeploy

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git fetch && git reset origin/main --hard
-docker compose -f ./src/docker-compose.yml down
-docker compose -f ./src/docker-compose.yml up -d --build
+docker-compose -f ./src/docker-compose.yml down
+docker-compose -f ./src/docker-compose.yml up -d --build
 
 exit 0
