df68fb22460adaee04ee1e9900cb10cf0ac66b3a
Manuel <manuelcota01@outlook.com>
2023-06-14 07:35:03 +0000

    Frontend scroll enabled and favicon

--- src/frontend/src/pages/_document.js
+++ src/frontend/src/pages/_document.js
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head rel="shortcut icon" href="/images/f1_logo_red.svg" type="image/svg+xml" />
       <body>
         <Main />
         <NextScript />
